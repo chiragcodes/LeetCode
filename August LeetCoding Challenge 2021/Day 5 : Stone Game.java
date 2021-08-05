d5af8a6acbd9a2056b4b6206f990142485a98dd8
Chirag Agrawal <15bce004@nirmauni.ac.in>
2021-08-05 12:38:06 +0530

    Create Day 5 : Stone Game.java

---
+++ August LeetCoding Challenge 2021/Day 5 : Stone Game.java
@@ -0,0 +1,14 @@
+class Solution {
+    
+    public boolean stoneGame(int[] piles) {
+        
+        /* Alex always wins.
+        
+        Reference: https://leetcode.com/problems/stone-game/discuss/154610/DP-or-Just-return-true
+        
+        */
+        
+        return true;
+        
+    }
+}
