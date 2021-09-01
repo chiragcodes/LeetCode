7bda73d3fa3c78c226418604ce0081c642af742e
Chirag Agrawal <15bce004@nirmauni.ac.in>
2021-09-01 14:41:36 +0530

    Create Day 1 : Array Nesting.java

---
+++ September LeetCoding Challenge 2021/Day 1 : Array Nesting.java
@@ -0,0 +1,35 @@
+class Solution {
+    
+    public int arrayNesting(int[] nums) {
+        
+        int n = nums.length;
+        int max = 0;
+        
+        for (int i = 0; i < n; i++) {
+            
+            int index = i, count = 0;
+            
+            while (nums[index] < n && nums[index] != i) {
+                
+                count++;
+                nums[index] = nums[index] + n;
+                index = nums[index] - n;
+                
+            }
+            
+            count++;
+            
+            if (count > max) {
+                max = count;
+            }
+            
+            if (max >= (n + 1) / 2) {
+                return max;
+            }
+                        
+        }
+        
+        return max;
+
+    }
+}
