0ef13b94a98e2b2bb2c58a6674fc124ac77941a1
Chirag Agrawal <15bce004@nirmauni.ac.in>
2021-09-05 12:51:14 +0530

    Create Day 5 : Orderly Queue.java

---
+++ September LeetCoding Challenge 2021/Day 5 : Orderly Queue.java
@@ -0,0 +1,28 @@
+class Solution {
+    
+    public String orderlyQueue (String s, int k) {
+        
+        if (k > 1) {
+            
+            char arr[] = s.toCharArray();
+            Arrays.sort(arr);
+            return new String(arr);
+            
+        }
+        
+        String res = s;
+        
+        for (int i = 1; i < s.length(); i++) {
+            
+            String temp = s.substring (i) + s.substring (0, i);
+            
+            if (res.compareTo(temp) > 0) {
+                res = temp;
+            }
+            
+        }
+        
+        return res;
+        
+    }
+}
